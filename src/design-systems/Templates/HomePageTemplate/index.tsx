493a499f10d41fde47b1482c91aba297aa15dde5
Deepak Verma <deepak@webllisto.com>
2024-06-24 11:42:24 +0530

    add google auth

--- src/design-systems/Templates/HomePageTemplate/index.tsx
+++ src/design-systems/Templates/HomePageTemplate/index.tsx
@@ -1,10 +1,26 @@
+'use client'
+
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+
 import Typography from 'design-systems/Atoms/Typography'
 import Layout from 'design-systems/Organisms/Layout'
 
 const HomePageTemplate: React.FC = () => {
   return (
     <Layout>
-      <Typography size="h1">Homepage</Typography>
+      <main>
+        <GoogleOAuthProvider clientId={'1080803887954-f5h0j9bmungd1qmt6eoc08odbi4ojb10.apps.googleusercontent.com'}>
+          <GoogleLogin
+            useOneTap
+            onError={() => {
+              console.info('Login Failed')
+            }}
+            onSuccess={(credentialResponse: any) => {
+              console.info(credentialResponse)
+            }}
+          />
+        </GoogleOAuthProvider>
+      </main>
     </Layout>
   )
 }
